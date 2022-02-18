13ca60fe3063a5e0713ce8bed50b000d7453d93f
elin_jsa <solrosen_96@hotmail.com>
2022-02-18 10:16:54 +0100

    visar NF,F,PF samt slider

--- Lab3_files/js/main.js
+++ Lab3_files/js/main.js
@@ -1,20 +1,61 @@
-/**
- *
-    Author: Kahin Akram
-    Date: Jan 24, 2020
-    TNM048 Lab 1 - Visual Information-Seeking Mantra
-    Main js file
- *
-*/
-//Passing data to the function
-var world_map, focus_plus_context, points
-d3.json("./data/ethqk.geojson", function (data) {
+/*
+var world_map
 
-    //Plotting
-    points = new Points();
-    //Working with the map
+d3.json("./data/geoData.geojson", function (data) {
     world_map = new worldMap(data);
-    //Working with the focus+context
-    focus_plus_context = new focusPlusContext(data);
-
 })
+*/
+
+var world_map
+
+//d3.csv("./data/oneYearAgg.csv", function(data) {
+d3.json("./data/oneYearAgg.json", function (data) {  
+
+    d3.json("./data/geoData.geojson", function (geo) {
+        
+       // world_map = new worldMap(geo, data);
+        world_map = new worldMap(geo, data);
+        //TESTAR geo
+        //console.log(geo.features) 
+        //console.log(geo.features[0])
+        //console.log(geo.features[0].properties.ADMIN) //Rätt!
+        //console.log(geo.features[1].properties.ADMIN) //Rätt!
+        console.log(Object.keys(data).length)
+        //Funkar ej att skriva ut alla listans ADMINs?
+        /*
+        for (var i = 0; i < geo.length; i++) {
+            console.log(geo.features[i].properties.ADMIN);
+        }
+        */
+        
+        //TESTAR data
+        
+       /* for (var i = 0; i < Object.keys(data).length; i++) {
+            //console.log(data[i].Status);
+            console.log(data[i].Country);
+        }*/
+        
+    })
+    
+})
+
+/*
+function map(){
+
+    d3.json("data/sweden.json", function(error, sweden) {
+
+        var counties = topojson.feature(sweden, sweden.objects.SWE_adm2).features;
+        
+        d3.csv("data/socialScienceUTF8.csv", function(error, data) {
+			// ❁ visar att CSVn är en js-array, ser ut som en json-fil ❁ 
+            //console.dir(data) 
+            draw(counties,data);
+        });
+
+    });
+
+    function draw(counties,data) {
+        ....
+    }
+}
+*/
